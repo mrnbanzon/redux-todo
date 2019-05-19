dae2022ddb1c1b6ef5b682277cd69c75ada569ed
Michael Banzon <mrnbanzon@gmail.com>
2019-05-18 20:40:29 -0700

    restructured AddTodo as a class component.

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,5 +1,28 @@
 import React from 'react';
 
-const AddTodo = () => <div>AddTodo</div>;
+class AddTodo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.textInput = React.createRef();
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log(this.textInput.current.value);
+    this.textInput.current.value = '';
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" ref={this.textInput} />
+          <input type="submit" value="ADD" />
+        </form>
+      </div>
+    );
+  }
+}
 
 export default AddTodo;
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,13 +3,17 @@ import AddTodo from './AddTodo.jsx';
 import TodoList from './TodoList.jsx';
 import Footer from './Footer.jsx';
 
-const App = () => (
-  <React.Fragment>
-    <h1>Todo List</h1>
-    <AddTodo />
-    <TodoList />
-    <Footer />
-  </React.Fragment>
-);
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Todo List</h1>
+        <AddTodo />
+        <TodoList />
+        <Footer />
+      </React.Fragment>
+    );
+  }
+}
 
 export default App;
